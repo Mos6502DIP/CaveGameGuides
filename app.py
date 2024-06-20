d8786e132587bb9bef395d07ed84c60a86f57d1f
Peter cakebread <peteriscakebread@gmail.com>
2024-06-20 12:53:20 +0100

    first

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_file, request, Response, redirect, render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def landing():
+    if request.method == 'POST':
+        user_name = request.form['username']
+        password = request.form['password']
+
+        return render_template("home.html", response="Entered the cum zone")
+    else:
+        return render_template(f"home.html", response="Null")
+
+
+if __name__ == '__main__':
+    app.run()
